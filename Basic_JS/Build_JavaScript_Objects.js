c1d073cccb6df68ce2fd659e2aa84c3347ba2702
Hossain K. M. <awarehossain@gmail.com>
2024-02-15 02:12:12 +0600

    cm-1

---
+++ Basic_JS/Build_JavaScript_Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+  // Only change code below this line
+  name: "Jacky",
+  legs: 2,
+  tails: 1,
+  friends: ["Tom", "Ran", "Jas"],
+
+  // Only change code above this line
+};
---
+++ Basic_JS/Manipulating_Complex_Objects.js
@@ -0,0 +1,16 @@
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+  {
+    artist: "Chester",
+    title: "Numb",
+    release_year: 2003,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+];
